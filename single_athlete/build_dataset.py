d3a70a799907762157db758173c43695a7877acc
RyanAugust <rad19206@yahoo.com>
2023-02-01 21:27:07 -0800

    performance metric calculations

--- single_athlete/build_dataset.py
+++ single_athlete/build_dataset.py
@@ -107,14 +107,120 @@ class dataset(object):
         return details
 
 class dataset_preprocess(object):
-    def __init__(self, athlete_statics):
-        self.
+    def __init__(self, local_activity_store=None, local_activity_model_params=None, athlete_statics=ryan_static_metrics):
+        self.athlete_statics = athlete_statics
+        self.local_activity_store = local_activity_store
+        self.local_activity_model_params = local_activity_model_params
 
+    
     def power_index_maker(self, power, duration, cp=340, w_prime=15000, pmax=448):
         theoretical_power = w_prime/duration - w_prime/(cp-pmax) + cp
         power_index = (power/theoretical_power)*100
         return power_index
 
+    def pre_process(self, performance_fxn, performance_lower_bound=0, sport=False):
+        data_df['xPace'] = np.where(data_df['xPace'] <= 0, data_df['Pace'], data_df['xPace'])
+        data_df = data_df[~(((data_df['Sport'] == 'Run') & (data_df['Pace'] <= 0))
+                | ((data_df['Sport'] == 'Bike') & (data_df['Average_Power'] <= 0))
+                | (data_df['Average_Heart_Rate'] <= 0))].copy()
+        data_df.rename(columns={'date':'workoutDate'}, inplace=True)
+        data_df['day_TSS'] = data_df['TSS'].groupby(data_df['workoutDate']).transform('sum').fillna(0)
+        data_df['performance_metric'] = data_df.apply(lambda row: performance_fxn(row, athlete_statics), axis=1)
+        # data_df['performance_metric'] = np.where(data_df['Duration'] < 60*60, 0, data_df['performance_metric'])
+        data_df['performance_metric'] = np.where(data_df['performance_metric'] < performance_lower_bound, 0, data_df['performance_metric'])
+        
+        # data_df = data_df[['workoutDate','day_TSS','performance_metric','Sport']]
+
+        data_df['performance_metric'] = data_df['performance_metric'].replace(0,np.nan)
+        data_df['performance_metric'] = data_df['performance_metric'].fillna(method='ffill')
+        agg_dict = {'day_TSS':'mean','performance_metric':'max'}
+        if sport:
+            agg_dict.update({'Sport':'first'})
+            data_df = data_df.sort_values('Sport')
+        data_df = data_df.groupby('workoutDate').agg(agg_dict)
+        
+        data_df['date'] = data_df.index
+        data_df['date'] = pd.to_datetime(data_df['date'])
+        data_df = data_df.sort_values(by=['date'])
+        data_df.index = pd.DatetimeIndex(data_df['date'])
+        missing_dates = pd.date_range(start=data_df.index.min(), end=data_df.index.max())
+        data_df = data_df.reindex(missing_dates, fill_value=0)
+        data_df['performance_metric'] = data_df['performance_metric'].replace(0,np.nan)
+        data_df['performance_metric'] = data_df['performance_metric'].fillna(method='ffill')
+        data_df = data_df.dropna()
+        return data_df
+
+class performance_functions(object):
+    def __init__(self):
+        self.name = 'perf fxns'
+        self.metric_function_map = {
+            'VO2':              self.calc_vo2,
+            'Garmin VO2':       self.use_garmin_vo2,
+            'AE EF':            self.calc_ae_ef,
+            'Power Index':      self.use_power_index,
+            'Power Index EF':   self.use_power_index_ef,
+            'Mod AE Power':     self.modeled_aerobic_threshold_power
+        }
+    
+    def derive_performance(self, activity_row, performance_metric):
+        performance_function = self.metric_function_map[performance_metric]
+        val = performance_function(activity_row)
+        return val
+
+    def calc_vo2(self, row):
+        if row['Sport'] == 'Bike':
+            percent_vo2 = (row['Average_Heart_Rate'] - athlete_statics["resting_hr"])/(athlete_statics["max_hr"] - athlete_statics["resting_hr"])
+            vo2_estimated = (((row['Average_Power']/75)*1000)/row['Athlete_Weight']) / percent_vo2
+        elif row['Sport'] == 'Run':
+            percent_vo2 = (row['Average_Heart_Rate'] - athlete_statics["resting_hr"])/(athlete_statics["max_hr"] - athlete_statics["resting_hr"])
+            vo2_estimated = (210/row['xPace']) / percent_vo2
+        else:
+            vo2_estimated =  0
+        return vo2_estimated
+
+    def use_garmin_vo2(self, row):
+        vo2_estimated = 0
+        if (row['Workout_Code'] != 'Rec') & (row['Sport'] in ['Run','Bike']):
+            vo2_estimated = row['VO2max_detected'] # Garmin VO2 Estimation
+        return vo2_estimated
+
+    def calc_ae_ef(self, row):
+        ef = 0
+        if (row['Workout_Code'] == 'AE'):
+            if row['Sport'] == 'Bike':
+                ef = row['IsoPower']/row['Average_Heart_Rate']
+            elif row['Sport'] == 'Run':
+                ef = row['IsoPower']/row['Average_Heart_Rate']
+        return ef
+
+    def use_power_index(self, row):
+        if row['Average_Power'] > 0:
+            val = row['Power_Index']
+        else:
+            val = 0
+        return val
+
+    def use_power_index_ef(self, row):
+        if row['Average_Power'] > 0:
+            hr_range = athlete_statics['max_hr'] - athlete_statics['resting_hr']
+            avg_hr_rel = row['Average_Heart_Rate'] - athlete_statics['resting_hr']
+            relative_hr = (avg_hr_rel / hr_range)*100
+            
+            pi_ef = row['Power_Index']/relative_hr
+            val = pi_ef
+        else:
+            val = 0
+        return val
+
+    def modeled_aerobic_threshold_power(self, row):
+        temp = 20
+        duration = 60*60
+        
+        if (row['a'] != 0) & (row['Duration'] > 999):
+            power = row['a'] + row['b']*athlete_statics['threshold_hr'] +  row['c']*duration*temp
+            return power
+        else:
+            return 0
 
 
 if __name__ == "__main__":
